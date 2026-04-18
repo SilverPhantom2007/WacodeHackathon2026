b53c244ef4fb1ccd46537e83e89ba4a78eb8e642
SilverPhantom2007 <75527152+SilverPhantom2007@users.noreply.github.com>
2026-04-18 15:05:30 -0500

    changes

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function AboutScreen() {
+
+  
+  function textChangeHandler(){
+    }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Phone Number</Text>
-      <TextInput>Input</TextInput>
+      <TextInput style={{height: 40, borderColor: 'white', borderWidth: 0, color:'#595959', fontSize:20, marginHorizontal:5}}
+        onChange={textChangeHandler}>Input</TextInput>
     </View>
   );
 }
